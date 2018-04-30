1509d1a77cec817f022afb96f888fff323f4da40
sangesherpa89 <37837913+sangesherpa89@users.noreply.github.com>
2018-04-30 15:32:15 -0400

    Update sketch.js

--- folder/switch/sketch.js
+++ folder/switch/sketch.js
@@ -1,22 +1,22 @@
 function setup() {
-  createCanvas(700, 400, WEBGL);
+  createCanvas(700, 400, WEBGL); //Create a canvas
   
 }
 function draw() {
-  background(205,211, 226);
+  background(205,211, 226); //sets the color used for the background
   fill(0);
   
-  if (mouseIsPressed) {
+  if (mouseIsPressed) { //shows ellipse shape when mouse is pressed
     var c = color(242, 12, 12); // Define color 'c'
     fill(c); // Use color variable 'c' as fill color
     noStroke(); // Don't draw a stroke around shapes
     ellipse(70,90,80,80); //Draw an ellipse
 
-  } else {
+  } else { //Shows rectangle when mouse is not pressed
     var c = color(11, 84, 253); // Define color 'c'
     fill(c); // Use color variable 'c' as fill color
     noStroke(); // Don't draw a stroke around shapes
     rect(85,85,90,80); //Draw a rectangle
   }
-  print(mouseIsPressed);
+  print(mouseIsPressed); //writes to the console area of your browser. 
   }
